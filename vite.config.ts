25f45dd37c5e6cefa417594bbc474400b858950e
Maxim Kardashin <mvkardashin@ya.ru>
2025-11-21 15:22:52 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const isGithubActions = !!process.env.GITHUB_ACTIONS
+const base = isGithubActions && repoName ? `/${repoName}/` : '/'
+
 export default defineConfig({
+  base,
   plugins: [react()],
 })
